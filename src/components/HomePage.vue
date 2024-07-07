80dd1b9bcb2c3b3197c72902cd22dc41c5f7d396
Anne Fernandes <annefernandeess@gmail.com>
2024-07-07 11:05:59 -0300

    creating firsts components

---
+++ src/components/HomePage.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="pills">
+        <PillComponent v-for="item in groupedWords" 
+            :word="item.word" :amount="item.amount"
+            :key="item.word">
+        </PillComponent>
+    </div>
+</template>
+
+<script>
+
+import PillComponent from './PillComponent.vue'
+
+export default {
+    components: {
+        PillComponent
+    },
+    data: function() {
+        return {
+            groupedWords: [
+                {word: "i", amount: 547},
+                {word: "you", amount: 478},
+                {word: "it", amount: 10}
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.home{
+    margin:20px;
+}
+.pills{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+</style>
